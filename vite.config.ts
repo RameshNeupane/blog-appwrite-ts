f2ef8dd52a282c38b7a5263783b01e21796168b5
RameshNeupane <neupaneramesh555@gmail.com>
2023-10-11 15:08:46 +0545

    add project setup

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@appwrite": path.resolve(__dirname, "./src/app/write/"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@config": path.resolve(__dirname, "./src/config"),
+            "@pages": path.resolve(__dirname, "./src/pages"),
+            "@store": path.resolve(__dirname, "./src/store"),
+        },
+    },
+});
