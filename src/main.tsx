64bd16053e9791d46d473a77fbb737b9796e8641
RameshNeupane <neupaneramesh555@gmail.com>
2023-10-12 19:09:22 +0545

    add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css";
+import router from "./router.tsx";
+import store from "@store/store.ts";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
+);
