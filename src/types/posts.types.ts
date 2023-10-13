86b066c18b823949d7b97735907df0c6bd895403
RameshNeupane <neupaneramesh555@gmail.com>
2023-10-13 15:14:05 +0545

    add types

---
+++ src/types/posts.types.ts
@@ -0,0 +1,29 @@
+import { Models } from "appwrite";
+
+export type PostFromDB = Models.Document;
+
+export interface IPostToDB {
+    title: string;
+    content: string;
+    featuredImage: string;
+    status: "inactive" | "active";
+    userId?: string;
+}
+
+export interface IPostFormData {
+    title: string;
+    slug: string;
+    content: string;
+    image: File;
+    status: "active" | "inactive";
+}
+
+export interface IAddNewPostData {
+    data: IPostFormData;
+    userId: string;
+}
+
+export interface IUpdatePostData {
+    post: PostFromDB;
+    data: IPostFormData;
+}
